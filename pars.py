b5db37244328c0b135304faac8cb4683d21a1bf6
ls-toast <leesak2004@gmail.com>
2019-08-22 23:59:07 +0900

    Add drone status parsing script

---
+++ pars.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+vehicle_state.py: 
+
+Demonstrates how to get and set vehicle state and parameter information, 
+and how to observe vehicle attribute (state) changes.
+
+Full documentation is provided at http://python.dronekit.io/examples/vehicle_state.html
+"""
+from __future__ import print_function
+from dronekit import connect, VehicleMode
+import time
+
+#Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle. 
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print("\nConnecting to vehicle on: %s" % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+vehicle.wait_ready('autopilot_version')
+
+ff=0
+
+while ff!=10:
+# Get all vehicle attributes (state)
+    print("\nGet all vehicle attribute values:")
+    print(" Autopilot Firmware version: %s" % vehicle.version)
+    print(" Global Location: %s" % vehicle.location.global_frame)
+    print(" Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+    print(" Local Location: %s" % vehicle.location.local_frame)
+    print(" Attitude: %s" % vehicle.attitude)
+    print(" Velocity: %s" % vehicle.velocity)
+    print(" GPS: %s" % vehicle.gps_0)
+    print(" Battery: %s" % vehicle.battery)
+    print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+    print(" Heading: %s" % vehicle.heading)
+    print(" Is Armable?: %s" % vehicle.is_armable)
+    print(" System status: %s" % vehicle.system_status.state)
+    print(" Groundspeed: %s" % vehicle.groundspeed)    # settable
+    print(" Airspeed: %s" % vehicle.airspeed)    # settable
+    print(" Mode: %s" % vehicle.mode.name)    # settable
+    print(" Armed: %s" % vehicle.armed)    # settable
+
+    ff+=1
+    time.sleep(1)
+    
+
+#Close vehicle object before exiting script
+print("\nClose vehicle object")
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl is not None:
+    sitl.stop()
+
+print("Completed")
+
+
+
+
